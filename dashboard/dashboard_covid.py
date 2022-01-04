ad210bf478494d08d82d517c2a2d4ea44a136344
parracode <parrarodriguezmiguelangel@gmail.com>
2022-01-04 12:30:16 +0100

    Feat: Tabla de bd y boton para descargar csv terminado a falta de conectar con mongo db y no con csv de local

--- dashboard/dashboard_covid.py
+++ dashboard/dashboard_covid.py
@@ -8,68 +8,120 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from st_aggrid import AgGrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+from st_aggrid.shared import JsCode
+from st_aggrid.shared import GridUpdateMode
+import plotly_express as px
 
 from utils.process_data import *
 
-from dashboard.data.get_data_covid import *
+from data.get_data_covid import *
 
-def app():
-    
+st.set_page_config(page_title="Covid compare", layout="wide") 
+
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
+  <a class="navbar-brand" href="https://www.linkedin.com/in/miguelangelparrarodriguez/" target="_blank">Miguel A. Parra</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://www.linkedin.com/in/miguelangelparrarodriguez/" target="_blank">LinkeIn</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://github.com/ParraCode" target="_blank">GitHub</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
+
+
     # ---------------------------------------------------------------------------------------------------------------------------------------
-    st.set_page_config(page_title="Compare Shows", layout="wide") 
+#st.set_page_config(page_title="Compare Shows", layout="wide") 
 
     # Division en columnas 
 
-    col1, col2= st.columns(2)
+col1, col2= st.columns(2)
 
     # ---------------------------------------------------------------------------------------------------------------------------------------
 
     # Titulo
 
-    col1.title('Core Code')
+col1.title('Core Code')
 
     # ---------------------------------------------------------------------------------------------------------------------------------------
 
     # Side Bar 
-    st.sidebar.header('1. Escoja un Contienente:',None)
+st.sidebar.header('1. Escoja un Contienente:',None)
 
-    list_contients = get_list_continent()
-    cotinent_selected = st.sidebar.selectbox('Selecciona tu continente',get_list_continent())
+cotinent_selected = st.sidebar.selectbox('Selecciona tu continente',get_list_continent())
 
-    st.sidebar.header('2. Escoja dos paises:',None)
-
-    # Lista de paises, y elijo un pais 
-    list_countrys1 = get_list_countrys_of_continent(cotinent_selected)
-    country_selected1 = st.sidebar.selectbox('Selecciona tu primer pais', list_countrys1)
+st.sidebar.header('2. Escoja dos paises:',None)
 
     # Lista de paises, y elijo un pais 
-    list_countrys2 = get_list_countrys_of_continent(cotinent_selected)
-    country_selected2 = st.sidebar.selectbox('Selecciona tu segundo pais', list_countrys2)
+list_countrys1 = get_list_countrys_of_continent(cotinent_selected)
+country_selected1 = st.sidebar.selectbox('Selecciona tu primer pais', list_countrys1)
 
-    st.sidebar.header('3. Escoja un periodo:',None)
+    # Lista de paises, y elijo un pais 
+list_countrys2 = get_list_countrys_of_continent(cotinent_selected)
+country_selected2 = st.sidebar.selectbox('Selecciona tu segundo pais', list_countrys2)
+
+st.sidebar.header('3. Escoja un periodo:',None)
 
 
-    st.sidebar.date_input('Fecha inicial', value=None, min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, kwargs=None)
+st.sidebar.date_input('Fecha inicial', value=None, min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, kwargs=None)
 
-    st.sidebar.date_input('Fecha final', value=None, min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, kwargs=None)
+st.sidebar.date_input('Fecha final', value=None, min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, kwargs=None)
 
 
-    # ---------------------------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------------------------
 
 
     # Dado el pais anterior obtengo los datos de confirmados para ese pais 
-    data_confirmed = get_data_confirmed_perd_day(cotinent_selected,country_selected1)
+data_confirmed = get_data_confirmed_perd_day(cotinent_selected,country_selected1)
 
     #  Proceso el json y lo hago dataframe 
-    df_country = fechaDay_int_transf(data_confirmed)
+df_country = fechaDay_int_transf(data_confirmed)
 
     # Meto el dataframe y ploteo 
-    col1.line_chart(data=df_country, width=0, height=0, use_container_width=True)
+col1.line_chart(data=df_country, width=0, height=0, use_container_width=True)
 
 
     #st.metric('label', df_country.max(), delta=None, delta_color="normal")
 
 
     # Mapa 
-    country_loc = country_location_coord(country_location())
-    st.map(country_loc)
+country_loc = country_location_coord(country_location())
+st.map(country_loc)
+
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+st.title("Covid DB shows analysis")
+
+shows = pd.read_csv("../data/CovidDB.csv")
+
+# add this
+gb = GridOptionsBuilder.from_dataframe(shows)
+
+gb.configure_pagination()
+gb.configure_side_bar()
+gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc="sum", editable=True)
+gb.configure_selection(selection_mode="multiple", use_checkbox=True)
+
+gridOptions = gb.build()
+
+data = AgGrid(shows, 
+              gridOptions=gridOptions, 
+              enable_enterprise_modules=True, 
+              allow_unsafe_jscode=True, 
+              update_mode=GridUpdateMode.SELECTION_CHANGED)
+st.download_button(label='Dowload CSV',data = shows.to_csv(), mime='text/csv')
--- dashboard/utils/process_data.py
+++ dashboard/utils/process_data.py
@@ -1,20 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def covid_cases_graph(data):
-    covid_cases_date = pd.DataFrame(data)
-    plt.figure(figsize=[20,10])
-    columns = [column for column in covid_cases_date.columns[1:]]
-    for country in covid_cases_date.values:
-        plt.ylabel('Cases')
-        plt.xlabel('Date')
-        plt.xticks(rotation=75)
-        plt.plot(columns, country[1:], label=country[0])
-        plt.legend()
-    return plt
-
 def fechaDay_int_transf (core_df):
-    import pandas as pd
     core_df = pd.DataFrame(core_df, columns = ['_id','date','totalConfirmed', 'totalDeaths'])
     core_df = core_df.drop(['_id'], axis=1)
     core_df['date'] = core_df['date'].apply(lambda x: list(x.values())[0][0:10])
@@ -23,7 +10,6 @@ def fechaDay_int_transf (core_df):
     return core_df
 
 def fechaYW_int_transf (core_df):
-    import pandas as pd
     core_df = pd.DataFrame(core_df, columns = ['_id', 'Year-Week','date','confirmedDay', 'deathsDay'])
     core_df = core_df.drop(['_id'], axis=1)
     core_df = core_df.groupby('Year-Week').sum()
@@ -31,7 +17,6 @@ def fechaYW_int_transf (core_df):
 
 
 def country_location_coord (core_df):
-    import pandas as pd
     core_df = pd.DataFrame(core_df, columns = ['latitude', 'longitude'])
     core_df['latitude'] = core_df['latitude'].apply(lambda x: list(x.values())[0]).astype(float)
     core_df['longitude'] = core_df['longitude'].apply(lambda x: list(x.values())[0]).astype(float)
